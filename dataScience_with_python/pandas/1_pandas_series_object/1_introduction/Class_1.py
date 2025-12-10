0001ec003a4ab13d1a1a778669ea3c96222fb2e2
rafael taparica <rafael_taparica@outlook.com>
2025-12-10 20:15:45 -0300

    update classes names

---
+++ dataScience_with_python/pandas/1_pandas_series_object/1_introduction/Class_1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# a Pandas object Series is one-dimensional array of indexed data.
+# can be created from a list or an array
+# Series combines a sequence of values with a sequence of indexes that we can
+# access with attributes values and index, values are a familiar numpy array, index is an object pd.index
+# of array type.
+
+data = pd.Series([0.25,0.5,0.75,1])
+values = data.values
+index = data.index
+print(data, '\n')
+print(values, '\n')
+print(index, '\n')
+
+# we can access data from associated index using [].
+print(data[2], '\n')
+
+print(data[0:2])
